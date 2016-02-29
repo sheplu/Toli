eaa290a025ec4f1269b1f9c854bc5537e9624c8b
Sheplu <jean.burellier@gmail.com>
2016-02-29 16:23:45 +0100

    update manifest and adding permission for internet

--- app/src/main/java/com/oromys/sheplu/toli/MainActivity.java
+++ app/src/main/java/com/oromys/sheplu/toli/MainActivity.java
@@ -2,8 +2,10 @@ package com.oromys.sheplu.toli;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -11,6 +13,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnTouchListener, View.OnClickListener {
@@ -24,12 +33,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().
+                permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         layout = (RelativeLayout) RelativeLayout.inflate(this,
                 R.layout.activity_main, null);
 
         text = (TextView) layout.findViewById(R.id.textView);
-        text.setText("Mon nouveau texte");
+        text.setText(readApi());
         text.setPadding(10, 100, 20, 150);
         text.setTextColor(Color.RED);
 
@@ -37,7 +49,6 @@ public class MainActivity extends AppCompatActivity
 
         button = (Button) layout.findViewById(R.id.button);
         button.setOnClickListener(this);
-
     }
 
     @Override
@@ -101,5 +112,50 @@ public class MainActivity extends AppCompatActivity
         intent.putExtra("note_paul", count.getText().toString());
         intent.putExtra("note_miche", 19);
         startActivity(intent);
+    }
+
+    public String readApi() {
+        InputStream in = null;
+        String result = null;
+
+        try {
+            URL url = new URL("http://financialproxy.herokuapp.com/");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream());
+            result = convert(in);
+        } catch (Exception e) {
+            Log.e("error", "readApi: ", e);
+        } finally {
+            if(in != null) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        return result;
+    }
+
+    public String convert(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        try {
+            while ( (line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (Exception e) {
+
+        } finally {
+            try {
+                is.close();
+            } catch (Exception e){
+
+            }
+        }
+
+        return sb.toString();
     }
 }
